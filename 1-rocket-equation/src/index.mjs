d6724baa5e6d66fb07ee1ae90323fbdd201e04ee
jovi De Croock <decroockjovi@gmail.com>
2019-12-01 18:16:57 +0100

    complete part 2

--- 1-rocket-equation/src/index.mjs
+++ 1-rocket-equation/src/index.mjs
@@ -3,14 +3,33 @@ import allParts from './input.mjs';
 const DIVISION = 3;
 const SUBTRACTION = 2;
 
+// Helpers
 const roundDown = (input) => Math.floor(input);
 const divideBy = (input, x) => input / x;
 const subtract = (input, x) => input - x;
+const calculateFuel = (input) => subtract(roundDown(divideBy(input, DIVISION)), SUBTRACTION);
 
-const calculateFuelRequirement = (parts) => parts.reduce((acc, part) => acc + subtract(roundDown(divideBy(part, DIVISION)), SUBTRACTION), 0);
+// Part 1
+const calculateTotalFuel = (parts) => parts.reduce((acc, part) => acc + calculateFuel(part), 0);
 
-console.log('test 1:', calculateFuelRequirement([12]), ' should be 2');
-console.log('test 2:', calculateFuelRequirement([14]), ' should be 2');
-console.log('test 3:', calculateFuelRequirement([1969]), ' should be 654');
-console.log('test 4:', calculateFuelRequirement([100756]), ' should be 33583');
-console.log('Final test ', calculateFuelRequirement(allParts));
+// Part 2
+const calculateFuelRec = (input, total = 0) => {
+  const fuelNeeded = calculateFuel(input);
+  if (fuelNeeded > 0) return calculateFuelRec(fuelNeeded , total + fuelNeeded)
+  return total;
+}
+
+const calculateTotalFuelRec = (parts) => parts.reduce((acc, part) => acc + calculateFuelRec(part), 0);
+
+console.log('--- PART 1 ---');
+console.log('test 1:', calculateTotalFuel([12]), ' should be 2');
+console.log('test 2:', calculateTotalFuel([14]), ' should be 2');
+console.log('test 3:', calculateTotalFuel([1969]), ' should be 654');
+console.log('test 4:', calculateTotalFuel([100756]), ' should be 33583');
+console.log('Final test ', calculateTotalFuel(allParts));
+console.log('--- PART 2 ---');
+console.log('test 1:', calculateTotalFuelRec([12]), ' should be 2');
+console.log('test 2:', calculateTotalFuelRec([14]), ' should be 2');
+console.log('test 3:', calculateTotalFuelRec([1969]), ' should be 966');
+console.log('test 4:', calculateTotalFuelRec([100756]), ' should be 50346');
+console.log('Final test ', calculateTotalFuelRec(allParts));
