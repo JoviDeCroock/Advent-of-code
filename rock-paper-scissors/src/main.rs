db7aa9e3b42fc52df4a6adcd6c75df1ddb6a581f
Jovi De Croock <decroockjovi@gmail.com>
2022-12-02 11:36:17 +0100

    better rust

--- rock-paper-scissors/src/main.rs
+++ rock-paper-scissors/src/main.rs
@@ -12,7 +12,45 @@ const POINTS_FOR_LOSS: i32 = 0;
 const POINTS_FOR_DRAW: i32 = 3;
 const POINTS_FOR_WIN: i32 = 6;
 
-fn get_move(input: &char) -> Option<Hand> {
+fn main() {
+    let contents = fs::read_to_string("./src/input.txt")
+        .expect("Should have been able to read the file");
+
+    let move_matrix = contents.split("\n").collect::<Vec<&str>>();
+
+    // Second column is your move
+    let mut score = 0;
+    move_matrix.iter().for_each(|item| {
+        let game = item.chars().collect::<Vec<char>>();
+
+        let opponent_move = translate_move(game.get(0).unwrap()).unwrap();
+        let my_move = translate_move(game.get(2).unwrap()).unwrap();
+
+        let outcome = get_outcome_for_game(&my_move, &opponent_move);
+        let total_score = get_score_for_move(&my_move) + get_score_for_outcome(&outcome);
+
+        score = score + total_score;
+    });
+
+    println!("Final score {:?}", score);
+
+    // Second column means what the outcome should be
+    let mut score = 0;
+    move_matrix.iter().for_each(|item| {
+        let game = item.chars().collect::<Vec<char>>();
+
+        let opponent_move = translate_move(game.get(0).unwrap()).unwrap();
+        let expected_outcome = translate_expected_outcome(game.get(2).unwrap()).unwrap();
+
+        let total_score = get_score_for_outcome(&expected_outcome) + get_score_for_move(&get_move_for_outcome(&expected_outcome, &opponent_move));
+
+        score = score + total_score;
+    });
+
+    println!("Final score {:?}", score);
+}
+
+fn translate_move(input: &char) -> Option<Hand> {
     match input {
         'A' | 'X' => Some(Hand::Rock),
         'B' | 'Y' => Some(Hand::Paper),
@@ -21,15 +59,7 @@ fn get_move(input: &char) -> Option<Hand> {
     }
 }
 
-fn get_score_for_move(input: &Hand) -> i32 {
-    match input {
-        Hand::Rock => POINTS_FOR_ROCK,
-        Hand::Paper => POINTS_FOR_PAPER,
-        Hand::Scissors => POINTS_FOR_SCISSORS,
-    }
-}
-
-fn get_expected_outcome_for_game(input: &char) -> Option<Result> {
+fn translate_expected_outcome(input: &char) -> Option<Result> {
     match input {
         'X' => Some(Result::Lose),
         'Y' => Some(Result::Draw),
@@ -46,77 +76,34 @@ fn get_score_for_outcome(input: &Result) -> i32 {
     }
 }
 
+fn get_score_for_move(input: &Hand) -> i32 {
+    match input {
+        Hand::Rock => POINTS_FOR_ROCK,
+        Hand::Paper => POINTS_FOR_PAPER,
+        Hand::Scissors => POINTS_FOR_SCISSORS,
+    }
+}
+
 fn get_outcome_for_game(my_move: &Hand, opponent_move: &Hand) -> Result {
-    let beaten_move = Beats::beats(my_move);
-    if my_move == opponent_move {
-        Result::Draw
-    } else if opponent_move == &beaten_move {
+    let own_winning_hand = Beats::beats(my_move);
+    let opponent_winning_hand = Beats::beats(opponent_move);
+
+    if my_move == &opponent_winning_hand {
+        Result::Lose
+    } else if opponent_move == &own_winning_hand {
         Result::Win
     } else {
-        Result::Lose
+        Result::Draw
     }
 }
 
-fn get_move_for_outcome(outcome: &Result, opponent_move: &Hand) -> Option<Hand> {
-    match opponent_move {
-        Hand::Rock => {
-            match outcome {
-                Result::Draw => Some(Hand::Rock),
-                Result::Win => Some(Hand::Paper),
-                Result::Lose => Some(Hand::Scissors),
-            }
-        },
-        Hand::Paper => {
-            match outcome {
-                Result::Draw => Some(Hand::Paper),
-                Result::Win => Some(Hand::Scissors),
-                Result::Lose => Some(Hand::Rock),
-            }
-        },
-        Hand::Scissors => {
-            match outcome {
-                Result::Draw => Some(Hand::Scissors),
-                Result::Win => Some(Hand::Rock),
-                Result::Lose => Some(Hand::Paper),
-            }
-        },
+fn get_move_for_outcome(outcome: &Result, opponent_move: &Hand) -> Hand {
+    let opponent_winning_hand = Beats::beats(opponent_move);
+    let own_winning_hand = Beats::beats(&opponent_winning_hand);
+
+    match outcome {
+        Result::Lose => opponent_winning_hand,
+        Result::Win => own_winning_hand,
+        Result::Draw => opponent_move.clone()
     }
-}
-
-fn main() {
-    let contents = fs::read_to_string("./src/input.txt")
-        .expect("Should have been able to read the file");
-
-    let move_matrix = contents.split("\n").collect::<Vec<&str>>();
-
-    // Second column is your move
-    let mut score = 0;
-    move_matrix.iter().for_each(|item| {
-        let game = item.chars().collect::<Vec<char>>();
-
-        let opponent_move = get_move(game.get(0).unwrap()).unwrap();
-        let my_move = get_move(game.get(2).unwrap()).unwrap();
-
-        let outcome = get_outcome_for_game(&my_move, &opponent_move);
-        let total_score = get_score_for_move(&my_move) + get_score_for_outcome(&outcome);
-
-        score = score + total_score;
-    });
-
-    println!("Final score {:?}", score);
-
-    // Second column means what the outcome should be
-    let mut score = 0;
-    move_matrix.iter().for_each(|item| {
-        let game = item.chars().collect::<Vec<char>>();
-
-        let opponent_move = get_move(game.get(0).unwrap()).unwrap();
-        let expected_outcome = get_expected_outcome_for_game(game.get(2).unwrap()).unwrap();
-
-        let total_score = get_score_for_outcome(&expected_outcome) + get_score_for_move(&get_move_for_outcome(&expected_outcome, &opponent_move).unwrap());
-
-        score = score + total_score;
-    });
-
-    println!("Final score {:?}", score);
 }
