f2f1689ca2555c6511b5277d8eea2c553df31345
Jovi De Croock <decroockjovi@gmail.com>
2023-12-01 10:02:10 +0100

    p2

--- Trebuchet/src/main.rs
+++ Trebuchet/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     let input = include_str!("../input.txt");
-    let sum = input.split('\n').into_iter().fold(0, |acc, line| {
-        // I need to find the first and last numerical characters and combine them into a single number
+    let part_1 = part_1(input);
+    println!("Part 1: {}", part_1);
+    let part_2 = part_2(input);
+    println!("Part 2: {}", part_2);
+}
+
+fn part_1(input: &str) -> i32 {
+    input.split('\n').into_iter().fold(0, |acc, line| {
         let numbers = line.chars().filter(|c| c.is_numeric()).collect::<Vec<char>>();
         if numbers.len() == 1 {
             let combined = format!("{}{}", numbers.get(0).unwrap(), numbers.get(0).unwrap()).parse::<i32>().unwrap();
@@ -12,7 +18,30 @@ fn main() {
         } else {
             return acc;
         }
-    });
+    })
+}
 
-    println!("Sum: {}", sum)
+fn part_2(text: &str) -> i32 {
+    text.split('\n').into_iter().fold(0, |acc, line| {
+        let line = line
+            .replacen("one", "on1e", 100)
+            .replacen("two", "tw2o", 100)
+            .replacen("three", "thr3ee", 100)
+            .replacen("four", "fo4ur", 100)
+            .replacen("five", "fi5ve", 100)
+            .replacen("six", "si6x", 100)
+            .replacen("seven", "sev7en", 100)
+            .replacen("eight", "eig8th", 100)
+            .replacen("nine", "ni9ne", 100);
+        let numbers = line.chars().filter(|c| c.is_numeric()).collect::<Vec<char>>();
+        if numbers.len() == 1 {
+            let combined = format!("{}{}", numbers.get(0).unwrap(), numbers.get(0).unwrap()).parse::<i32>().unwrap();
+            return acc + combined;
+        } else if numbers.len() > 1 {
+            let combined = format!("{}{}", numbers.get(0).unwrap(), numbers.get(numbers.len() - 1).unwrap()).parse::<i32>().unwrap();
+            return acc + combined;
+        } else {
+            return acc;
+        }
+    })
 }
